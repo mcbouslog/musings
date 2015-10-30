4db2f08a6ec704f3074d85862fd60eab33a029b5
mcbouslog <mcbouslog@gmail.com>
2015-10-30 07:44:28 -0500

    update to all index

--- public/assets/javascript/controllers/allIndexController.js
+++ public/assets/javascript/controllers/allIndexController.js
@@ -1,9 +1,6 @@
 angular.module('MarkMusings').controller('AllIndexController', function(TV, Podcast, $scope) {
 
-	if ($scope.allItems === undefined) {
-		$scope.allItems = TV.all;
-		var podcasts = Podcast.all;
-		Array.prototype.push.apply($scope.allItems, podcasts);	
-	}
+	$scope.tvs = TV.all;
+	$scope.podcasts = Podcast.all;
 
 });
